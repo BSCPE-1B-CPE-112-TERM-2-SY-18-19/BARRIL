dc9bfc604d6bf3182bc5ffff5240c9b64a9c9b8c
Cathbarril26 <47840822+CathleneBarril@users.noreply.github.com>
2019-03-08 17:21:09 +0800

    Update Passing Array to a function in C++ Programming.cpp

--- Passing Array to a function in C++ Programming.cpp
+++ Passing Array to a function in C++ Programming.cpp
@@ -1,22 +1,65 @@
-#include<iostream>
-
+#include <iostream>
 using namespace std;
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-void display (int marks[10]);
-
-int main()
+class wasteful{  			  /*Nag declare kog class nga wasteful paramao rasad ni akong pagatawagon sa main */
+	public: 
+		void display(int m[5]);
+		long digit[100],n, x, t, j, factor[100], rem[100], num[10],num1[10];  	//Function nga gideclare ang mga variables para gamiton sa ubos nga code para makuha ang output nga wasteful number */
+};
+int main() 
 {
-	int marks[10] = { 20, 22, 24, 26, 28, 30, 32, 34, 36, 38};
-	display(marks);
+	int num[10]={21,22,23,24,25,26,27,28,29,30};    /*Array ni sya nga gisudlan og numbers kaning mga numbera ang pagabasihan kung asay mga wasteful number */
+	wasteful numbers;					/* Mao na sya ang gitawag na */
+	numbers.display (num);					/* Pangan sa akong object */
 	return 0;
 }
 
-void display(int m[10])
+	void wasteful::display(int num[10]) 		/*Mao ni akong pagatawagon */
 {
-	cout<< "Displaying marks: "<< endl;
+	cout<<"Displaying WASTEFUL: "<<endl;	
 	
-	for(int i=0;i<10;++i)
+	for (int i=0; i<10; i++)			/*sugod sa code para sa output nga or sugod sa looping para sa output nga wasteful number */
 	{
-		cout<<"Student"<<i+1<<" : "<<m[i]<<endl;
-	}
+		
+	    for (i=0;i<10; i++)
+			{
+			digit[i]=0;
+			n=num[i];
+			x=n;
+			for (;n!=0;)
+				{
+				n=n/10;
+				digit[i]++;
+				}
+			t=0;
+			j=2;
+			factor[i]=0;
+			for (;x!=0;)
+				{
+				if (x%j==0)
+					{
+					x=x/j;
+					rem[t]=j;
+					t++;
+					factor[i]++;
+					if(x==1)
+						{
+						break;
+						}
+					}
+			else
+				{
+				j++;
+				}
+			}
+		}
+		for (i=0; i<10; i++)
+			{
+			if (factor[i]>digit[i])
+				{
+				cout<<"Number: "<<num[i]<<endl;
+				}
+			}
+		}
 }
